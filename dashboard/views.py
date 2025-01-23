5924ffe78afb3a26027b5e92016041fb50f35542
Samandar Nayimjonov <SamandarNayimjonov@gmail.com>
2025-01-23 14:07:41 +0300

    ALl done Dashboardni tugrladim

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,9 +8,13 @@ def home(request):
     products = Product.objects.all()
     categories = Category.objects.all()
     orders = Order.objects.all()
+    category_names = [category.name for category in Category.objects.all()]
+    category_product_counts = [category.products.count() for category in Category.objects.all()]
     ctx = {
         'products': products,
         'categories': categories,
         'orders': orders,
+        'category_names': category_names,
+        'category_product_counts': category_product_counts,
     }
     return render(request, 'dashboard/index.html', ctx)
